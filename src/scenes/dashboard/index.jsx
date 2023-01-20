0ce82c967ecece27d02a2c7a5539d00b4110e007
therogersak <therogersak@gmail.com>
2023-01-20 19:22:09 +0530

    first commit

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
+
+function Dashboard() {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center"></Box>
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+    </Box>
+  );
+}
+
+export default Dashboard;
